f4ebd4f29a893970b939876fb8d6c8c20bee2947
Shair <jimmy_jhair17@hotmail.com>
2026-02-01 02:01:14 -0500

    setup project

---
+++ src/vite-env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  // readonly VITE_APP_TITLE?: string;
+  // readonly VITE_API_URL: string;
+  readonly VITE_API_BASE_URL: string;
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
